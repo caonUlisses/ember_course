2143ac24cfb4a5ec41a81f35132d3d2570c5365f
Ulisses Caon <ulissescaon@gmail.com>
2018-03-12 19:53:52 -0300

    Standard everything

--- app/models/day.js
+++ app/models/day.js
@@ -1,8 +1,7 @@
-import DS from 'ember-data';
+import DS from 'ember-data'
 
 export default DS.Model.extend({
   date: DS.attr('string'),
-  value: attr('boolean', { default: false }),
+  value: DS.attr('boolean', { default: false }),
   calendar: DS.belongsTo('calendar')
-});
-
+})
